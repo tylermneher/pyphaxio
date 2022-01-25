f0e0da09c3508d8a54df6a05a95c0e81908841bd
Tyler M. Neher <tylermneher@users.noreply.github.com>
2022-01-25 11:37:52 -0500

    created pyphaxio.py

---
+++ pyphaxio.py
@@ -0,0 +1,8 @@
+import Credentials
+
+api_key = Credentials.API_KEY
+api_secret = Credentials.API_SECRET
+fax_no_phaxio = Credentials.FAX_NO_PHAXIO
+fax_no_tyler = Credentials.FAX_NO_DIRECT
+
+api_url = "https://api.phaxio.com/v2.1"
